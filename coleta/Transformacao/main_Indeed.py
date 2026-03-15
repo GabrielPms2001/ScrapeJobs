84c806defe4e9f78bea3f74cb5d4d400bf843433
user.name <gabrielpms2001@gmail.com>
2026-03-15 19:06:18 -0300

    Salvando cod de tratamento

--- coleta/Transformacao/main_Indeed.py
+++ coleta/Transformacao/main_Indeed.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 DATA_DIR = BASE_DIR / "data"
-JSON_PATH = DATA_DIR / "Vagas_Linkedin.jsonl"
+JSON_PATH = DATA_DIR / "vaga_indeed.jsonl"
 
 if not JSON_PATH.exists():
     raise FileNotFoundError(f"Arquivo não encontrado: {JSON_PATH}")
@@ -35,26 +35,26 @@ df = pd.read_json(JSON_PATH, lines=True)
 # =============================
 
 df['_data_coleta'] = datetime.now()
-df['_Plataforma'] = "Linkedin"
+df['_Plataforma'] = "Indeed"
 
 # =============================
 # 6. TRATAMENTO DE DADOS
 # =============================
 
-df["Categoria"] = df["vaga_name"].fillna("Sem categoria").astype(str)
-df["Titulo"] = df["title"].fillna("Sem título").astype(str)
-df["Empresa"] = df["company"].fillna("Sem empresa").astype(str)
-df["Local"] = df["location"].fillna("Sem localização").astype(str)
-df["Data_de_postagem"] = pd.to_datetime(df["date_posted"], errors="coerce")
-df["Link"] = df["job_link"].fillna("Sem link").astype(str)
-df["Imagem_Logo"] = df["Logo_image"].fillna("Sem imagem").astype(str)
+df["Categoria"] = df["cargo_busca"].fillna("Sem categoria").astype(str)
+df["Titulo"] = df["titulo"].fillna("Sem título").astype(str)
+df["Empresa"] = df["empresa"].fillna("Sem empresa").astype(str)
+df["Local"] = df["local"].fillna("Sem localização").astype(str)
+df["Data_de_postagem"] = None
+df["Link"] = df["link"].fillna("Sem link").astype(str)
+df["Imagem_Logo"] = None
 
 # =============================
 # 7. AJUSTES FINAIS
 # =============================
 
 df = df.drop(
-    columns=["vaga_name", "title", "company", "location", "date_posted", "job_link", "Logo_image"],
+    columns=["cargo_busca", "titulo", "local", "date_posted", "link", "Logo_image", "empresa"],
     errors="ignore"
 )
 
@@ -65,7 +65,7 @@ df = df.drop(
 df.to_sql(
     "Vagas",          # nome da tabela
     engine,              # conexão SQL Server
-    if_exists="replace", # replace ou append
+    if_exists="append", # replace ou append -> replace apaga a tabela e cria uma nova, append adiciona os dados na tabela existente
     index=False
 )
 
